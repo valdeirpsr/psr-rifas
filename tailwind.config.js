4edf1f4ca2b2331f78e4c25af1e7fa7a792a6cb3
Valdeir S <valdeirpsr@hotmail.com.br>
2023-06-22 15:17:36 -0300

    build: adiciona suporte ao tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.ts",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
