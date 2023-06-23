37b6b2bd39be90ef1768ea5f8e7454ed3c606647
Valdeir S <valdeirpsr@hotmail.com.br>
2023-06-23 10:12:36 -0300

    build: adiciona suporte ao Inertia e Vue3 para o front-end

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue()
     ],
 });
