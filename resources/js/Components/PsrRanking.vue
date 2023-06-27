d96fbe1391e4deb114e0f14693dc235081aa9b00
Valdeir S <valdeirpsr@hotmail.com.br>
2023-06-27 09:51:27 -0300

    feat: cria componente PsrRanking
    O componente mostrará o ranking dos 3 maiores compradores.
    
    closes VAL-17

---
+++ resources/js/Components/PsrRanking.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import { computed, readonly } from 'vue';
+  import { RankingType } from '../Types/Ranking';
+  import PsrCard from './PsrCard.vue';
+
+  const props = defineProps<{
+    users: RankingType[];
+  }>();
+
+  const ranking = computed(() => {
+    const users = Object.assign({}, props.users);
+    return users.sort((prev, cur) => (prev.total < cur.total ? 1 : -1)).slice(0, 3);
+  });
+
+  const medals = readonly(['🥇', '🥈', '🥉']);
+</script>
+
+<template>
+  <PsrCard>
+    <template #heading> Ranking dos compradores </template>
+
+    <div class="flex flex-wrap justify-around gap-4 mx-auto sm:max-w-xl text-center">
+      <div
+        v-for="(user, index) in ranking"
+        :key="`ranking-${user.total}`"
+        :class="index === 0 ? 'flex-initial w-full sm:flex-auto sm:w-auto' : 'flex-none sm:flex-auto'"
+        data-test="ranking-user"
+      >
+        <span class="text-[32px]">{{ medals.at(index) }}</span>
+        <p class="text-sm mt-3">
+          {{ user.name }}<br />
+          <b>{{ user.total }}</b> bilhetes
+        </p>
+      </div>
+    </div>
+  </PsrCard>
+</template>
