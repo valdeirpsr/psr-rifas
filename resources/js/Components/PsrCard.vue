65baad5eef31f647a633751f744af215238308f9
Valdeir S <valdeirpsr@hotmail.com.br>
2023-06-27 09:12:14 -0300

    feat: cria o componente PsrCard
    Ele será responsável por renderizar as seções da página, como por
    exemplo: ranking, reservar bilhetes etc.
    
    closes VAL-15

---
+++ resources/js/Components/PsrCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="border border-gray-300 bg-white p-4 rounded-2xl space-y-4 sm:p-6">
+    <p
+      v-if="$slots.heading"
+      class="uppercase text-center font-bold font-[Raleway] text-xs sm:text-sm"
+    >
+      <slot name="heading" />
+    </p>
+    <slot />
+  </div>
+</template>
