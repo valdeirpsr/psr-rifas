f69180d66babd20cb598a8b7eb3b7f6656efb824
Valdeir S <valdeirpsr@hotmail.com.br>
2023-06-27 12:09:40 -0300

    feat: adiciona composables para formatar dados
    useLocaleDateLong: formata data para o padrão "21 de junho de 2023 às 00:00"
    
    useLocaleCurrency: formata moedas para o estilo BRL

---
+++ resources/js/Composables/Locale.ts
@@ -0,0 +1,20 @@
+import { toValue } from "@vueuse/core";
+
+export function useLocaleDateLong(value: string|Date) {
+    const date = value instanceof Date ? toValue(value) : new Date(toValue(value));
+
+    return date.toLocaleDateString('pt-br', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+}
+
+export function useLocaleCurrency(value: number) {
+    return toValue(value).toLocaleString('pt-br', {
+        currency: 'BRL',
+        style: 'currency',
+    }).replace(/\s/g, '')
+}
