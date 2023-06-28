a4c4335293f20b715c85b46ccc7612a5273fdc4d
Valdeir S <valdeirpsr@hotmail.com.br>
2023-06-28 14:26:40 -0300

    chore: adiciona ícone SVG do WhatsApp

--- resources/assets/icons/index.ts
+++ resources/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export * as IconSvgEmail from './email.svg?component';
 export * as IconSvgPeople from './people.svg?component';
 export * as IconSvgTelephone from './telephone.svg?component';
+export * as IconSvgWhatsApp from './whatsapp.svg?component';
