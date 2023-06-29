b8ada6992d814f8f6a8455e42302f56821b49fdd
Valdeir S <valdeirpsr@hotmail.com.br>
2023-06-29 12:54:48 -0300

    feat: cria o componente PsrCountdown
    O componente renderizará uma contagem regressiva. Usado para indicar o tempo para um pedido pendente ser expirado.
    
    closes VAL-27

---
+++ resources/js/Components/PsrCountdown.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { computed } from 'vue';
+  import VueCountdown from '@chenfengyuan/vue-countdown';
+
+  const props = defineProps<{
+    time: Date|number|string
+  }>();
+
+  const time = computed((): number => {
+    if (props.time instanceof Date) {
+      return props.time.getTime() - Date.now();
+    }
+
+    if (typeof props.time === 'string') {
+      return new Date(props.time).getTime() - Date.now();
+    }
+
+    return props.time;
+  })
+</script>
+
+<template>
+  <VueCountdown :time="time" v-slot="{ totalHours, minutes, seconds }">
+    <slot>{{ totalHours }} horas, {{ minutes }} minutos e {{ seconds }} segundos</slot>
+  </VueCountdown>
+</template>
