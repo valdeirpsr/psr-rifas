66fa67805c27c356e47a201804d9eea611342f62
Valdeir S <valdeirpsr@hotmail.com.br>
2023-07-04 10:25:35 -0300

    feat: adiciona o node-snackbar
    Dependência para exibir pequenos avisos de erro ou sucesso para os usuários

---
+++ resources/js/Composables/Snackbar.ts
@@ -0,0 +1,5 @@
+import { show } from 'node-snackbar';
+import 'node-snackbar/dist/snackbar.min.css';
+
+export const useDanger = (text: string) =>
+  show({ text, backgroundColor: '#f87272', textColor: '#000', actionTextColor: '#fff' })
