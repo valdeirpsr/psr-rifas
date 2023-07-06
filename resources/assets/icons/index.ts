9d55083adeb3c7ffd8486e520c23d57b118433f3
Valdeir S <valdeirpsr@hotmail.com.br>
2023-07-06 08:41:59 -0300

    chore: adiciona ícone do Pix em SVG

--- resources/assets/icons/index.ts
+++ resources/assets/icons/index.ts
@@ -2,3 +2,4 @@ export * as IconSvgEmail from './email.svg?component';
 export * as IconSvgPeople from './people.svg?component';
 export * as IconSvgTelephone from './telephone.svg?component';
 export * as IconSvgWhatsApp from './whatsapp.svg?component';
+export * as IconSvgPix from './pix.svg?component';
