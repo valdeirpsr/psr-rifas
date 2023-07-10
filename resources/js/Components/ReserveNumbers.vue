3cfc4632a0d0c152aad43eab102259aaf49d49c0
Valdeir S <valdeirpsr@hotmail.com.br>
2023-07-10 16:15:34 -0300

    refactor: substitui o sistema de contador em ReserveNumbers
    O VueUse já conta com uma composable que facilita a criação do contador.

--- resources/js/Components/ReserveNumbers.vue
+++ resources/js/Components/ReserveNumbers.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-  import { computed, ref } from 'vue';
+  import { computed, watch } from 'vue';
+  import { useCounter } from '@vueuse/core';
   import { useLocaleCurrency } from '../Composables/Locale';
 
   import PsrCard from './PsrCard.vue';
@@ -16,25 +17,14 @@
     buyMin: number;
   }>();
 
-  const quantity = ref(props.buyMin);
+  const { count, inc, dec } = useCounter(props.buyMin, { max: props.buyMax, min: props.buyMin });
 
-  const priceTotal = computed(() => useLocaleCurrency(props.price * quantity.value));
+  const priceTotal = computed(() => useLocaleCurrency(props.price * count.value));
 
-  function increment(n = 1) {
-    quantity.value = Math.min(quantity.value + n, props.buyMax);
-    preventLetters();
-  }
-
-  function decrement() {
-    quantity.value = Math.max(quantity.value - 1, props.buyMin);
-    preventLetters();
-  }
-
-  function preventLetters() {
-    quantity.value = parseFloat(quantity.value.toString().replace(/\D/g, '') || '1');
-
-    emits('update:quantity', quantity.value);
-  }
+  watch(count, (newValue) => {
+    if(!newValue) count.value = 1;
+    emits('update:quantity', count.value);
+  })
 </script>
 
 <template>
@@ -42,29 +32,28 @@
     <template #heading>Selecione a quantidade de bilhetes</template>
 
     <div class="grid grid-cols-4 gap-2 w-full mx-auto sm:max-w-xl">
-      <button class="button-qntd" data-testid="quantity-1" @click="increment(1)">+1</button>
-      <button class="button-qntd" data-testid="quantity-10" @click="increment(10)">+10</button>
-      <button class="button-qntd" data-testid="quantity-50" @click="increment(50)">+50</button>
-      <button class="button-qntd" data-testid="quantity-100" @click="increment(100)">+100</button>
+      <button class="button-qntd" data-testid="quantity-1" @click="inc(1)">+1</button>
+      <button class="button-qntd" data-testid="quantity-10" @click="inc(10)">+10</button>
+      <button class="button-qntd" data-testid="quantity-50" @click="inc(50)">+50</button>
+      <button class="button-qntd" data-testid="quantity-100" @click="inc(100)">+100</button>
     </div>
 
     <div class="flex justify-between gap-2 mx-auto w-full sm:max-w-xl">
-      <button class="button-qntd" data-testid="decrement" @click="decrement">-</button>
+      <button class="button-qntd" data-testid="decrement" @click="dec(1)">-</button>
       <input
-        v-model="quantity"
+        v-model="count"
         type="number"
         class="border border-gray-300 rounded-md text-center flex-1 w-full"
         data-testid="quantity"
         :aria-label="`Escolha quantos números você quer comprar. Mínimo: ${buyMin}. Máximo: ${buyMax}`"
-        @input="preventLetters"
       />
-      <button class="button-qntd" data-testid="increment" @click="increment()">+</button>
+      <button class="button-qntd" data-testid="increment" @click="inc()">+</button>
     </div>
 
     <p class="w-full mx-auto text-end text-sm sm:max-w-xl" data-testid="price-total">Valor total: {{ priceTotal }}</p>
 
     <div class="w-full max-w-xl mx-auto">
-      <PsrButton class="w-full" data-testid="confirm" @click="$emit('reserveNumbers', quantity)">
+      <PsrButton class="w-full" data-testid="confirm" @click="$emit('reserveNumbers', count)">
         Reservar Bilhetes
       </PsrButton>
     </div>
