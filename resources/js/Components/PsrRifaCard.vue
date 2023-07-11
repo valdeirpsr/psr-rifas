3eb126c176206d7c6735fd7fc28a1c7d01c11005
Valdeir S <valdeirpsr@hotmail.com.br>
2023-07-11 16:06:47 -0300

    feat: cria componente de Rifa PsrRifaCard
    Cria um componente para centralizar a visualização da rifa. Este componente
    renderizará informações simples como título, preço e thumbnail. Será
    utilizado na página inicial e lista de rifas finalizadas.
    
    closes VAL-52

---
+++ resources/js/Components/PsrRifaCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { useLocaleCurrency, useLocaleDateLong } from '@Composables/Locale';
+  import { computed } from 'vue';
+
+  const props = defineProps<{
+    rifa: Rifa;
+  }>();
+
+  const expiredAt = computed(() => (props.rifa.expired_at ? useLocaleDateLong(props.rifa.expired_at) : ''));
+  const price = computed(() => (props.rifa.price ? useLocaleCurrency(props.rifa.price) : ''));
+</script>
+
+<template>
+  <a :href="route('rifas.show', [rifa.slug])" class="block h-48 rounded overflow-hidden">
+    <img alt="ecommerce" class="object-cover object-center w-full h-full block" :src="rifa.thumbnail" />
+  </a>
+  <div class="mt-4">
+    <p v-if="rifa.expired_at" class="text-gray-500 text-xs mb-1">Expira em: {{ expiredAt }}</p>
+    <h1 class="text-gray-900 text-lg font-bold">
+      <a :href="route('rifas.show', [rifa.slug])">{{ rifa.title }}</a>
+    </h1>
+    <p class="mt-1">{{ price }}</p>
+  </div>
+</template>
