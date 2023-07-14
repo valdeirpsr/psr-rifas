2290e32c5601526ef6b706b2e16a57084674c889
Valdeir S <valdeirpsr@hotmail.com.br>
2023-07-14 14:37:56 -0300

    tests: adiciona testes para criação de rifa através do filament

---
+++ tests/Feature/Filament/RifaResourceTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature\Filament;
+
+use App\Filament\Resources\RifaResource;
+use App\Models\Rifa;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class RifaResourceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_a_rifa_deve_ser_criada_com_dados_basicos(): void
+    {
+        $rifa = Rifa::factory()->make();
+
+        $form = [
+            'title' => $rifa->title,
+            'thumbnail' => [$rifa->thumbnail],
+            'price' => $rifa->price,
+            'total_numbers_available' => $rifa->total_numbers_available,
+            'description' => $rifa->description,
+        ];
+
+        Livewire::test(RifaResource\Pages\CreateRifa::class)
+            ->fillForm($form)
+            ->call('create')
+            ->assertHasNoFormErrors();
+
+        $this->assertDatabaseHas('rifas', [
+            ...$form,
+            /* Valores padrão */
+            'buy_max' => 300,
+            'buy_min' => 1,
+            'status' => 'draft'
+        ]);
+    }
+
+    /**
+     * São valores obrigatórios:
+     *  - title
+     *  - thumbnail
+     *  - price
+     */
+    public function test_a_rifa_nao_deve_ser_criada_sem_valores_obrigatorios(): void
+    {
+        $rifa = Rifa::factory()->make();
+
+        Livewire::test(RifaResource\Pages\CreateRifa::class)
+            ->fillForm([])
+            ->call('create')
+            ->assertHasFormErrors([
+                'title' => 'required',
+                'thumbnail' => 'required',
+                'price' => 'required'
+            ]);
+    }
+}
