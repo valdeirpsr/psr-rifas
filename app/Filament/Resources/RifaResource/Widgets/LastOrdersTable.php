0f473128185f99092458009fcc3b91d7ad9822e6
Valdeir S <valdeirpsr@hotmail.com.br>
2023-11-11 16:19:06 -0300

    feat: cria widget para listar tabela dos pedidos mais recentes
    O widget será mostrando quando o administrador acessar a página de
    informações da rifa, através do painel de controle via Filament

---
+++ app/Filament/Resources/RifaResource/Widgets/LastOrdersTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\RifaResource\Widgets;
+
+use App\Enums\OrderStatus;
+use App\Models\Order;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Support\Str;
+
+class LastOrdersTable extends BaseWidget
+{
+    protected static ?string $heading = 'Pedidos Recentes';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Order::query()->latest())
+            ->columns([
+                TextColumn::make('id')
+                    ->label('ID'),
+                TextColumn::make('customer_fullname')
+                    ->label('Nome do Comprador'),
+                TextColumn::make('customer_telephone')
+                    ->label('Telefone'),
+                TextColumn::make('numbers_reserved')
+                    ->label('Qdte. de Números')
+                    ->formatStateUsing(fn ($state) => Str::of($state)->explode(',')->count() ),
+                TextColumn::make('status')
+                    ->label('Situação')
+                    ->badge()
+                    ->formatStateUsing(fn (?string $state) => OrderStatus::tryFrom($state)->getLabel())
+                    ->color(fn (?string $state) => OrderStatus::tryFrom($state)->getColor()),
+            ])
+            ->actions([
+                EditAction::make()
+                    ->url(fn (Order $order) => url("/admin/orders/{$order->id}/edit")),
+            ]);
+    }
+}
