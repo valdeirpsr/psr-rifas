618ca01d5da43a45821bcb29d84d9ac924d479dc
Valdeir S <valdeirpsr@hotmail.com.br>
2023-11-11 17:49:16 -0300

    feat: cria a página ViewOrder para o OrderResource

---
+++ app/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\Pages;
+
+use App\Filament\Resources\OrderResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewOrder extends ViewRecord
+{
+    protected static string $resource = OrderResource::class;
+}
