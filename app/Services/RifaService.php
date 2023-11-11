6c5ef258d4f31df1a35c90db257be84062dac30f
Valdeir S <valdeirpsr@hotmail.com.br>
2023-11-11 16:29:32 -0300

    feat: cria função no RifaService para retornar o ranking de compradores
    Com isso, é possível verificar o usuário que mais contribuiu e,
    possívelmente, dar um prêmio

--- app/Services/RifaService.php
+++ app/Services/RifaService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 use App\Enums\OrderStatus;
 use App\Models\Order;
 use App\Models\Rifa;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class RifaService
 {
@@ -11,5 +13,19 @@ class RifaService
     {
         return $rifa->orders()->where('status', $orderStatus)
             ->count('id');
+    }
+
+    public function getRanking(Rifa $rifa): Collection
+    {
+        return Order::query()->select([
+            'customer_fullname',
+            'customer_telephone',
+            DB::raw('SUM(JSON_LENGTH(numbers_reserved)) AS total')
+        ])
+        ->where('status', OrderStatus::PAID)
+        ->where('rifa_id', $rifa->id)
+        ->groupBy('customer_telephone')
+        ->orderBy('total', 'desc')
+        ->get();
     }
 }
