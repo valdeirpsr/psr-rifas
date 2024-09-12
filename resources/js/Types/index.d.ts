968fd98827aa35e24c96b33aef695a8be0a85068
Valdeir S <valdeirpsr@hotmail.com.br>
2024-09-11 22:33:57 -0300

    style: padroniza código typescript e vue com prettier

--- resources/js/Types/index.d.ts
+++ resources/js/Types/index.d.ts
@@ -1,79 +1,79 @@
 declare type Order = {
-  id: number,
-  customer_fullname: string,
-  customer_email: string,
-  customer_telephone: string,
-  numbers_reserved: string[],
-  status: string,
-  expire_at: string|null,
-  created_at: string,
-  updated_at: string,
-  rifa_id: number
-}
+  id: number;
+  customer_fullname: string;
+  customer_email: string;
+  customer_telephone: string;
+  numbers_reserved: string[];
+  status: string;
+  expire_at: string | null;
+  created_at: string;
+  updated_at: string;
+  rifa_id: number;
+};
 
 declare type Payment = {
-  ticket_url: string,
-  order_id: number,
-  id: number
-}
+  ticket_url: string;
+  order_id: number;
+  id: number;
+};
 
 declare type OrderWithPayment = Order & {
-  payment: Payment|null
-}
+  payment: Payment | null;
+};
 
 declare type OrderStatuses = {
-  expired: string,
-  paid: string,
-  reserved: string,
-  unknown: string,
-  [k: string]: string
-}
+  expired: string;
+  paid: string;
+  reserved: string;
+  unknown: string;
+  [k: string]: string;
+};
 
 declare type Ranking = {
-  customer_fullname: string,
-  total_numbers: number
-}
+  customer_fullname: string;
+  total_numbers: number;
+};
 
 declare type Rifa = {
-  id : number,
-  title: string,
-  thumbnail: string,
-  price: number,
-  description: string,
-  slug: string,
-  total_numbers_available: number,
-  buy_max: number,
-  buy_min: number,
-  raffle: string,
-  status: 'draft' | 'published' | 'finished',
-  expired_at: string|null,
-  created_at: string,
-  updated_at: string
-}
+  id: number;
+  title: string;
+  thumbnail: string;
+  price: number;
+  description: string;
+  slug: string;
+  total_numbers_available: number;
+  buy_max: number;
+  buy_min: number;
+  raffle: string;
+  status: 'draft' | 'published' | 'finished';
+  expired_at: string | null;
+  created_at: string;
+  updated_at: string;
+};
 
 declare type FormReserveNumbers = {
-  fullname: string,
-  email: string,
-  telephone: string,
-  confirmTelephone: string,
-  terms: boolean,
-  quantity: number,
-  rifa: Rifa['id']
-}
+  fullname: string;
+  email: string;
+  telephone: string;
+  confirmTelephone: string;
+  terms: boolean;
+  quantity: number;
+  rifa: Rifa['id'];
+};
 
 declare type Slideshow = {
-  image: string,
-  alt: string,
-}
+  image: string;
+  alt: string;
+};
 
 declare type Winner = {
-  customer_fullname: Order['customer_fullname'],
-  video: string|null,
-  position: number
-}
+  customer_fullname: Order['customer_fullname'];
+  video: string | null;
+  position: number;
+};
 
 declare type Testimonial = {
-  id: number,
-  testimonial: string,
-  order: Pick<Order, 'customer_fullname'>
-}
+  id: number;
+  testimonial: string;
+  order: Pick<Order, 'customer_fullname'>;
+};
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,17 +6,17 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue, Ziggy)
+      .mount(el);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
 
 import './bootstrap';
